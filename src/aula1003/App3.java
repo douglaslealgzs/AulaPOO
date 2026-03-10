b646ffcff1e477f0e5959dc7350d58be2ca18c22
DOUGLAS LEAL GONCALVES DE QUEIROZ <douglas.queiroz000@cs.unicid.edu.br>
2026-03-10 20:41:05 -0300

    TerceiroProjetoPOO

--- src/aula1003/App3.java
+++ src/aula1003/App3.java
@@ -1,17 +1,23 @@
 package aula1003;
 
-public class App3 {
+import java.util.Scanner;
 
+public class App3 {
+	//Entre com um nr. e informe quantoas vezes ele se repete
+	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int contador = 4;
 		int valor[] = {10,47,78,99,7,41,42,21,4,14,4,4,4,4,1,1,4,1,2,};
 		int soma = 0;
 		for(int i=0; i<valor.length; i++) {
-			System.out.println(valor[0]);
+			
 			 soma += valor[i];
 			
 
 		}
-		System.out.println("soma total: " + soma);
+		System.out.println("Repetiu " + contador + "vezes ");
 
 	}
 
