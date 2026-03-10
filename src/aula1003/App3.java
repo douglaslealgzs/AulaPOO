0f6d9c3637e90d5d74967ac9d88ab01e19200b68
DOUGLAS LEAL GONCALVES DE QUEIROZ <douglas.queiroz000@cs.unicid.edu.br>
2026-03-10 20:28:46 -0300

    TerceiroProjetoPOO

--- src/aula1003/App3.java
+++ src/aula1003/App3.java
@@ -3,7 +3,15 @@ package aula1003;
 public class App3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int valor[] = {10,47,78,99,7};
+		int soma = 0;
+		for(int i=0; i<5; i++) {
+			System.out.println(valor[0]);
+			 soma += valor[i];
+			
+
+		}
+		System.out.println("soma total: " + soma);
 
 	}
 
