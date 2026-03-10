808075475da447e5e0fb285984e1d5be2457d3cd
DOUGLAS LEAL GONCALVES DE QUEIROZ <douglas.queiroz000@cs.unicid.edu.br>
2026-03-10 20:30:46 -0300

    TerceiroProjetoPOO

--- src/aula1003/App3.java
+++ src/aula1003/App3.java
@@ -3,9 +3,9 @@ package aula1003;
 public class App3 {
 
 	public static void main(String[] args) {
-		int valor[] = {10,47,78,99,7};
+		int valor[] = {10,47,78,99,7,41,42,21,4,14,4,4,4,4,1,1,4,1,2,};
 		int soma = 0;
-		for(int i=0; i<5; i++) {
+		for(int i=0; i<valor.length; i++) {
 			System.out.println(valor[0]);
 			 soma += valor[i];
 			
